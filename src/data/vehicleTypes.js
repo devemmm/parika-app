e66597bc91a3704cc207c1b8e74e86255ad58b5e
iamayala <ndimayala@gmail.com>
2021-04-07 15:08:08 +0200

    added - fake api for vehicle types

---
+++ src/data/vehicleTypes.js
@@ -0,0 +1,38 @@
+export const vehicleTypes = [
+    {
+        id: '034',
+        type: 'sedan'
+    },
+    {
+        id: '035',
+        type: 'coupe'
+    },
+    {
+        id: '036',
+        type: 'sports car'
+    },
+    {
+        id: '037',
+        type: 'station wagon'
+    },
+    {
+        id: '038',
+        type: 'hatchback'
+    },
+    {
+        id: '039',
+        type: 'convertible'
+    },
+    {
+        id: '040',
+        type: 'sports utility vehicle'
+    },
+    {
+        id: '041',
+        type: 'minivan'
+    },
+    {
+        id: '042',
+        type: 'pickup truck'
+    }
+]
