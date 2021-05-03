e2dc5990ac9b4d0ece2e40c82a0f8f8a35af3d5c
iamayala <ndimayala@gmail.com>
2021-05-03 11:10:36 +0200

    added loader

---
+++ src/screens/LoginScreens/Loading.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import { View, ActivityIndicator, StyleSheet } from 'react-native'
+import { theme_green } from '../../constants/constants'
+
+export class Loading extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             loading: true
+        }
+    }
+
+    componentDidMount(){
+        this.handleNext()
+    }
+
+    handleNext = () => {
+        setTimeout(() => {
+            this.props.navigation.navigate('MainAppNav')
+        }, 3000);
+    }
+    
+    render() {
+        if (this.state.loading) {
+            return (
+            <View style={styles.Container}>
+                <ActivityIndicator size="large" color={theme_green} />
+            </View>
+            )            
+        }
+    }
+}
+
+export default Loading
+const styles = StyleSheet.create({
+    Container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
